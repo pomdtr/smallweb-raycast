a870b068a08608653781e60d2a119cfde56f4ee1
pomdtr <achille.lacoin@gmail.com>
2024-12-14 16:52:21 +0100

    update the extension for publishing it in the store

--- README.md
+++ README.md
@@ -1,44 +1,6 @@
 # Smallweb Raycast Extension
 
-![Smallweb Raycast Extension](./metadata/smallweb-1.png)
+## Usage
 
-## Install
-
-1. `npm install`
-2. `npm run dev` (while raycast is running)
-
-## Installing self-signed certificates
-
-If you're using `*.localhost` domains, you'll need to tell raycast to use caddy self-signed certificates.
-
-You can configure them in the raycast Advanced settings.
-
-![alt text](media/image.png)
-
-If you installed caddy using homebrew, you can find the root certificate at `/opt/homebrew/var/lib/caddy/pki/authorities/local/root.crt`.
-
-## Configuration
-
-1. Make sure that the api is published on your smallweb instance
-
-    ```jsonc
-    // ~/smallweb/api/smallweb.json
-    {
-        "entrypoint": "smallweb:api",
-        "private": true,
-        "publicRoutes": [
-            "/openapi.json",
-            "/schemas/*"
-        ]
-    }
-    ```
-
-2. Generate a token for the app
-
-    ```bash
-    smallweb token create --app api --description "raycast extension"
-    ```
-
-3. Pass both the url and the token to the extension at start
-
-4. (Optional) If smallweb is synced to a folder on your desktop, you can configure it in the extension settings
+- Make sure that your smallweb directory is synced on your local machine.
+- When you first start the extension, you will be prompted to select the path to your smallweb directory.
--- src/search-apps.tsx
+++ src/search-apps.tsx
@@ -1,98 +1,64 @@
-import { Action, ActionPanel, Clipboard, Detail, getPreferenceValues, Icon, Image, Keyboard, List } from "@raycast/api";
-import { getFavicon, usePromise, useFrecencySorting } from "@raycast/utils";
-import * as client from "./client";
-import type { FileStat } from "webdav"
+import { Action, ActionPanel, Detail, getPreferenceValues, Icon, Image, Keyboard, List } from "@raycast/api";
+import { getFavicon, usePromise } from "@raycast/utils";
+import fs from "fs/promises";
 import path from "path";
 
-const { dir } = getPreferenceValues<Preferences.SearchApps>()
+const prefs = getPreferenceValues<Preferences.SearchApps>()
+
+type Config = {
+    domain: string
+}
+
+async function loadConfig(dir: string): Promise<Config> {
+    const configPath = path.join(dir, ".smallweb", "config.json")
+    const stat = await fs.stat(configPath).catch(() => null)
+    if (!stat) {
+        throw new Error("Config file not found")
+    }
+
+    const content = await fs.readFile(configPath, "utf8")
+    const config = JSON.parse(content) as Config
+    return config
+}
+
+async function listApps(dir: string) {
+    let entries = await fs.readdir(dir)
+    const names = entries.filter(entry => !entry.startsWith("."))
+
+    const config = await loadConfig(dir)
+    return names.map(name => ({
+        name,
+        url: `https://${name}.${config.domain}`,
+        dir: path.join(dir, name)
+    }))
+}
+
 
 export default function SearchApps() {
-    const { data, isLoading } = usePromise(async () => {
-        const resp = await client.api["/v0/apps"].get()
-        if (!resp.ok) {
-            throw new Error(resp.statusText)
-        }
+    const { data: apps, isLoading, error } = usePromise(listApps, [prefs.dir])
 
-        return resp.json()
-    })
+    if (error) {
+        return <Detail markdown={error.message} />
+    }
 
-    const { data: apps, visitItem } = useFrecencySorting(data, {
-        key: app => app.name,
-    });
 
     return <List isLoading={isLoading}>{
         apps?.map(app => <List.Item icon={getFavicon(app.url, {
-            fallback: Icon.Globe,
             mask: Image.Mask.RoundedRectangle,
-        })} key={app.name} keywords={[app.name]} title={app.manifest?.short_name || app.name} subtitle={app.manifest?.description} accessories={[{ text: app.name }]} actions={
+            fallback: Icon.Globe,
+        })} key={app.name} keywords={[app.name]} title={app.name} accessories={[{ text: app.url }]} actions={
             <ActionPanel>
                 <ActionPanel.Section>
-                    <Action.OpenInBrowser title="Open in Browser" url={app.url} onOpen={() => visitItem(app)} />
-                    <Action.Push icon={Icon.Folder} title="Browse Files" target={<BrowseDir path={`/${app.name}`} />} />
-                    <Action.CopyToClipboard shortcut={Keyboard.Shortcut.Common.Copy} title="Copy Link" content={app.url} onCopy={() => visitItem(app)} />
+                    <Action.OpenInBrowser title="Open in Browser" url={app.url} />
+                    {/* <Action.Push icon={Icon.Folder} title="Browse Files" target={<BrowseDir path={`/${app.name}`} />} /> */}
+                    <Action.CopyToClipboard shortcut={Keyboard.Shortcut.Common.Copy} title="Copy Link" content={app.url} />
                 </ActionPanel.Section>
-                {dir ?
-                    <ActionPanel.Section>
-                        <Action.Open shortcut={Keyboard.Shortcut.Common.Open} application="Finder" title="Open in finder" target={path.join(dir, app.name)} />
-                        <Action.OpenWith shortcut={Keyboard.Shortcut.Common.OpenWith} path={path.join(dir, app.name)} onOpen={() => visitItem(app)} />
-                        <Action.CopyToClipboard shortcut={Keyboard.Shortcut.Common.CopyPath} title="Copy Path" content={path.join(dir, app.name)} onCopy={() => visitItem(app)} />
-                    </ActionPanel.Section> : null}
+                <ActionPanel.Section>
+                    <Action.Open shortcut={Keyboard.Shortcut.Common.Open} application="Finder" title="Open in finder" target={app.dir} />
+                    <Action.OpenWith shortcut={Keyboard.Shortcut.Common.OpenWith} path={app.dir} />
+                    <Action.CopyToClipboard shortcut={Keyboard.Shortcut.Common.CopyPath} title="Copy Path" content={app.dir} />
+                </ActionPanel.Section>
             </ActionPanel>
         } />)
     }</List>
-}
-
-function BrowseDir(props: { path: string }) {
-    const { data: entries, isLoading } = usePromise(async () => {
-        return client.webdav.getDirectoryContents(props.path) as Promise<FileStat[]>
-    })
-
-    return <List isLoading={isLoading}>{
-        entries?.map(entry => <List.Item icon={entry.type == "directory" ? Icon.Folder : Icon.Document} key={entry.filename} title={entry.basename} accessories={[{ text: entry.type }]} actions={
-            <ActionPanel>
-                {entry.type === "directory" ?
-                    <ActionPanel.Section>
-                        <Action.Push icon={Icon.Folder} title="Browse Files" target={<BrowseDir path={entry.filename} />} />
-                    </ActionPanel.Section>
-                    :
-                    <ActionPanel.Section>
-                        <Action.Push title="View File" target={<ViewFile entry={entry} />} />
-                        <FileActions entry={entry} />
-                    </ActionPanel.Section>
-                }
-            </ActionPanel>
-
-        } />)
-    }</List>
-}
-
-function FileActions({ entry }: { entry: FileStat }) {
-    return <>
-        {dir ? <Action.Open shortcut={Keyboard.Shortcut.Common.Open} title="Open" icon={Icon.Document} target={path.join(dir, entry.filename)} /> : null}
-        {dir ? <Action.OpenWith shortcut={Keyboard.Shortcut.Common.OpenWith} path={path.join(dir, entry.filename)} /> : null}
-        {dir ? <Action.CopyToClipboard shortcut={Keyboard.Shortcut.Common.CopyPath} title="Copy Path" content={path.join(dir, entry.filename)} /> : null}
-        <Action shortcut={Keyboard.Shortcut.Common.Copy} icon={Icon.Clipboard} title="Copy Content" onAction={async () => {
-            const content = await client.webdav.getFileContents(entry.filename, { format: "text" }) as string
-            await Clipboard.copy(content)
-        }} />
-    </>
-}
-
-
-
-function ViewFile({ entry }: { entry: FileStat }) {
-    const { data: content, isLoading } = usePromise(async () => {
-        return client.webdav.getFileContents(entry.filename, { format: "text" }) as Promise<string>
-    })
-    const extension = path.extname(entry.filename) || "txt"
-
-    return <Detail isLoading={isLoading} markdown={content ? codeblock(content, extension.slice(1)) : undefined} actions={
-        <ActionPanel>
-            <FileActions entry={entry} />
-        </ActionPanel>
-    } />
-}
-
-function codeblock(code: string, lang: string) {
-    return "```" + lang + "\n" + code + "\n```"
 }
