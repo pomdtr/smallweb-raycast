7614d60345c9193773dd164e6c741cbe17f4fabe
pomdtr <achille.lacoin@gmail.com>
2024-12-14 16:54:45 +0100

    run prettier

--- src/search-apps.tsx
+++ src/search-apps.tsx
@@ -3,62 +3,80 @@ import { getFavicon, usePromise } from "@raycast/utils";
 import fs from "fs/promises";
 import path from "path";
 
-const prefs = getPreferenceValues<Preferences.SearchApps>()
+const prefs = getPreferenceValues<Preferences.SearchApps>();
 
 type Config = {
-    domain: string
-}
+  domain: string;
+};
 
 async function loadConfig(dir: string): Promise<Config> {
-    const configPath = path.join(dir, ".smallweb", "config.json")
-    const stat = await fs.stat(configPath).catch(() => null)
-    if (!stat) {
-        throw new Error("Config file not found")
-    }
+  const configPath = path.join(dir, ".smallweb", "config.json");
+  const stat = await fs.stat(configPath).catch(() => null);
+  if (!stat) {
+    throw new Error("Config file not found");
+  }
 
-    const content = await fs.readFile(configPath, "utf8")
-    const config = JSON.parse(content) as Config
-    return config
+  const content = await fs.readFile(configPath, "utf8");
+  const config = JSON.parse(content) as Config;
+  return config;
 }
 
 async function listApps(dir: string) {
-    let entries = await fs.readdir(dir)
-    const names = entries.filter(entry => !entry.startsWith("."))
+  const entries = await fs.readdir(dir);
+  const names = entries.filter((entry) => !entry.startsWith("."));
 
-    const config = await loadConfig(dir)
-    return names.map(name => ({
-        name,
-        url: `https://${name}.${config.domain}`,
-        dir: path.join(dir, name)
-    }))
+  const config = await loadConfig(dir);
+  return names.map((name) => ({
+    name,
+    url: `https://${name}.${config.domain}`,
+    dir: path.join(dir, name),
+  }));
 }
 
-
 export default function SearchApps() {
-    const { data: apps, isLoading, error } = usePromise(listApps, [prefs.dir])
+  const { data: apps, isLoading, error } = usePromise(listApps, [prefs.dir]);
 
-    if (error) {
-        return <Detail markdown={error.message} />
-    }
+  if (error) {
+    return <Detail markdown={error.message} />;
+  }
 
-
-    return <List isLoading={isLoading}>{
-        apps?.map(app => <List.Item icon={getFavicon(app.url, {
+  return (
+    <List isLoading={isLoading}>
+      {apps?.map((app) => (
+        <List.Item
+          icon={getFavicon(app.url, {
             mask: Image.Mask.RoundedRectangle,
             fallback: Icon.Globe,
-        })} key={app.name} keywords={[app.name]} title={app.name} accessories={[{ text: app.url }]} actions={
+          })}
+          key={app.name}
+          keywords={[app.name]}
+          title={app.name}
+          accessories={[{ text: app.url }]}
+          actions={
             <ActionPanel>
-                <ActionPanel.Section>
-                    <Action.OpenInBrowser title="Open in Browser" url={app.url} />
-                    {/* <Action.Push icon={Icon.Folder} title="Browse Files" target={<BrowseDir path={`/${app.name}`} />} /> */}
-                    <Action.CopyToClipboard shortcut={Keyboard.Shortcut.Common.Copy} title="Copy Link" content={app.url} />
-                </ActionPanel.Section>
-                <ActionPanel.Section>
-                    <Action.Open shortcut={Keyboard.Shortcut.Common.Open} application="Finder" title="Open in finder" target={app.dir} />
-                    <Action.OpenWith shortcut={Keyboard.Shortcut.Common.OpenWith} path={app.dir} />
-                    <Action.CopyToClipboard shortcut={Keyboard.Shortcut.Common.CopyPath} title="Copy Path" content={app.dir} />
-                </ActionPanel.Section>
+              <ActionPanel.Section>
+                <Action.OpenInBrowser title="Open in Browser" url={app.url} />
+                {/* <Action.Push icon={Icon.Folder} title="Browse Files" target={<BrowseDir path={`/${app.name}`} />} /> */}
+                <Action.CopyToClipboard shortcut={Keyboard.Shortcut.Common.Copy} title="Copy Link" content={app.url} />
+              </ActionPanel.Section>
+              <ActionPanel.Section>
+                <Action.Open
+                  shortcut={Keyboard.Shortcut.Common.Open}
+                  application="Finder"
+                  title="Open in Finder"
+                  target={app.dir}
+                />
+                <Action.OpenWith shortcut={Keyboard.Shortcut.Common.OpenWith} path={app.dir} />
+                <Action.CopyToClipboard
+                  shortcut={Keyboard.Shortcut.Common.CopyPath}
+                  title="Copy Path"
+                  content={app.dir}
+                />
+              </ActionPanel.Section>
             </ActionPanel>
-        } />)
-    }</List>
+          }
+        />
+      ))}
+    </List>
+  );
 }
